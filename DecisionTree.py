6d1848b227a9f26646f315e362e386f1b234df5e
aaron <aaronwong668@gmail.com>
2019-09-19 16:58:14 -0700

    best split using giniimpurity and infogain done

--- DecisionTree.py
+++ DecisionTree.py
@@ -6,29 +6,25 @@ class DecisionTree:
 
     # read csv file and store dimensions
     def __init__(self, file):
-        mat = np.genfromtxt(file,dtype=None, skip_header=1, delimiter=",", autostrip=True, case_sensitive=False)
+        mat = np.genfromtxt(file,dtype=int, skip_header=1, delimiter=",", autostrip=True, case_sensitive=False)
         # get header
         self.header = np.genfromtxt(file,dtype=str,delimiter=",", autostrip=True, max_rows=1)
         # header remove
         self.header[0] = "age"
-        self.header[2] = "chest pain type"
-        self.header[3] = "resting blood pressure"
-        
-        np.delete(mat, 0, axis=0) 
-       
         self.X = mat
         dims = mat.shape
         self.rows = dims[0]
         self.columns = dims[1]
+        # need to split for training and validation on self.X
 
     # gini impurity in a nutshell is the probaility of predicting the wrong class in subset vector y (random chance)
     def giniImpurity(self, X):
-        # since we are predicting whether someone has recieved treatment (yes/no) there are only 2 classes
+        # since we are predicting whether someone is male or female there are only 2 classes
         allClasses = {}
-        # tally up counts of yes and no
+        # tally up counts of female and male (male = 1, female = 0)
         for row in X:
-            # vector y will be column 7 in survey.csv
-            classVal = row[7]
+            # label vector y will be column 1 in heart.csv
+            classVal = row[1]
             if classVal not in allClasses:
                 allClasses[classVal] = 1
             else:
@@ -40,16 +36,38 @@ class DecisionTree:
             impurity -= probOfC**2
         return impurity
     
+    def findBestSplit(self, rows):
+        bestInfoGain = 0
+        bestQuestion = None
+        # currentGiniUncertainty - weightedAvgGiniUncertainty = infogain
+        currentGiniUncertainty = self.giniImpurity(rows)
+        # 0,1,2,3...13
+        for colIndex in range(self.columns):
+            # get all unique column values at current colIndex
+            uniqueVals = set([row[colIndex] for row in rows])
+            for uniqueVal in uniqueVals:
+                quest = Question.Question(colIndex, uniqueVal)
+                trueRows, falseRows = self.partition(rows, quest)
+                if len(trueRows) == 0 or len(falseRows) == 0:
+                    continue
+                trueGini = self.giniImpurity(trueRows)
+                falseGini = self.giniImpurity(falseRows)
+                infoGain = currentGiniUncertainty - (trueGini + falseGini)
+                if infoGain > bestInfoGain:
+                    bestInfoGain = infoGain
+                    bestQuestion = quest
+        return bestInfoGain, bestQuestion
+
     # split on dataset
-    def partition(self, question):
+    def partition(self, allRows, question):
         true, false = [], []
-        for row in self.X:
-            if question.askQuestion:
+        for row in allRows:
+            if question.askQuestion(row):
                 true.append(row)
             else:
                 false.append(row)
         return true, false
-    
+
 dT = DecisionTree("heart.csv")
 gini = dT.giniImpurity(dT.X)
 print("value is " + str(gini))
--- Question.py
+++ Question.py
@@ -2,12 +2,16 @@
 
 class Question:
     def __init__(self, col, val):
+        # index in row
         self.attribute = col
+        # save the reference value we are comparing against
         self.value = val
     
-    def askQuestion(self):
-        attributeValue = self.attribute
+    def askQuestion(self, row):
+        # get the value of at index
+        attributeValue = row[self.attribute]
         if isinstance(attributeValue, int) or isinstance(attributeValue, float):
+            # check if the row value is greater or = to the reference value
             return self.attribute >= self.value
         else:
             # string case
@@ -16,6 +20,8 @@ class Question:
     def __repr__(self):
         attributeValue = self.attribute
         if isinstance(attributeValue, int) or isinstance(attributeValue, float):
+            # if number
             return "Is %s %s %s" % (self.attribute, ">=", self.value)
         else:
+            # if string
             return "Is %s %s %s" % (self.attribute, "==", self.value)
